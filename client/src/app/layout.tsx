ec11c2bf5761b960226a51319e011cea23db6384
alrigo123 <alexestrada2804@gmail.com>
2025-07-20 17:16:33 -0500

    fixed margin

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,19 +5,15 @@ import "./globals.css";
 // IMPORT CONSISTENT COMPONENTS
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
       <body className="body flex flex-col min-h-screen" suppressHydrationWarning>
-
-        <SessionProvider>
-          <Navbar />
-          <main className="main flex-1 container mx-auto">{children}</main>
-          <Footer />
-        </SessionProvider>
+        <Navbar />
+        <main className="main flex-1 w-full">{children}</main>
+        <Footer />
       </body>
     </html>
   );
